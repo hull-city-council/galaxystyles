f5a54301263b14554d510274c8f79517fff69b2e
HCCDanny <117913283+HCCDanny@users.noreply.github.com>
2023-11-22 14:18:21 +0000

    Create partnership-logos.js

---
+++ liveithull/partnership-logos.js
@@ -0,0 +1,10 @@
+document.querySelector("body > footer > div > div.site-footer__right").innerHTML += 
+`
+  <div class="partner-heading">
+  <p style="margin-bottom: unset;color:#fff">Working in partnership</p>
+  </div>
+  <div class="partner-container">
+  		<img class="partner-logos" style="max-width: 100px;" src="/downloads/file/69/hcandl" alt="Hull Culture and Leisure Logo" loading="lazy">
+  		<img class="partner-logos" style="max-width: 200px;" src="/downloads/file/68/hcc" alt="Hull City Council Logo" loading="lazy">
+  </div>
+`;
