c96a227d6b4bbd12645c29d6f4e86f7ac824c3d0
HCCPete <112623301+HCCPete@users.noreply.github.com>
2024-04-04 16:22:59 +0100

    Update reciteme.js

--- museum collection/reciteme.js
+++ museum collection/reciteme.js
@@ -66,7 +66,7 @@ recitemeButtonLink.appendChild(document.createTextNode("Accessibility and transl
 recitemeButtonLinkMobile.appendChild(document.createTextNode("Accessibility and translation"));
 
 /* document.getElementById("nav").appendChild(recitemeButtonLink); */
-document.getElementsByClassName("page-content")[0].appendChild(recitemeButtonLink);
+document.getElementsByClassName("page-header__content")[0].appendChild(recitemeButtonLink);
 //document.getElementsByClassName("nav").appendChild(recitemeButtonLink);
 
 
