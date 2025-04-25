f12477fdb7f8a5969e3b047f705482570f33edc4
HCCPete <112623301+HCCPete@users.noreply.github.com>
2025-04-25 08:35:20 +0100

    Update reciteme.js

--- changingfutures/reciteme.js
+++ changingfutures/reciteme.js
@@ -53,7 +53,7 @@ define = function(a, b, c) {
 	}
 	almondDefine(a, b, c);
 };
-const recitemeButtonLink = document.createElement("a"),
+const recitemeButtonLink = document.createElement("a");
 recitemeButtonLinkMobile = document.createElement("a");
 recitemeButtonLink.setAttribute("id", "enableRecite");
 recitemeButtonLink.setAttribute("role", "button");
@@ -61,7 +61,7 @@ recitemeButtonLink.setAttribute("class", "recite_button");
 recitemeButtonLinkMobile.setAttribute("class", "link depth-1");
 recitemeButtonLinkMobile.setAttribute("id", "enableReciteM");
 recitemeButtonLinkMobile.setAttribute("role", "button");
-recitemeButtonLink.appendChild(document.createTextNode("Accessibility and translation"));
+recitemeButtonLink.appendChild(document.createTextNode("Accessibility2 and translation"));
 recitemeButtonLinkMobile.appendChild(document.createTextNode("Accessibility and translation"));
 document.getElementById("nav").appendChild(recitemeButtonLink);
 document.getElementById("navPanel").appendChild(recitemeButtonLinkMobile);
