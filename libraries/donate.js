d63fd6cc607ce1986c53a92c6d0e8c76ed1c24b9
HCCDanny <117913283+HCCDanny@users.noreply.github.com>
2025-05-12 08:34:19 +0100

    Update donate.js

--- libraries/donate.js
+++ libraries/donate.js
@@ -4,7 +4,7 @@ function createDonateButton(id) {
 
     const link = document.createElement('a');
     link.setAttribute('id', `${id}-donate-button`);
-    link.setAttribute('href', 'https://hullcc-self.test.achieveservice.com/en/AchieveForms/?form_uri=sandbox-publish://AF-Process-111d38a4-5c4a-4c49-a2d4-259190734abe/AF-Stage-3bf77b12-b84e-4866-a55c-6773f6e4b343/definition.json&redirectlink=/en&cancelRedirectLink=/en');
+    link.setAttribute('href', '//hullcc-self.achieveservice.com/service/Library_donation');
     link.setAttribute('title', 'Donate');
     link.innerHTML = '<span class="button__text">Donate</span>';
 
